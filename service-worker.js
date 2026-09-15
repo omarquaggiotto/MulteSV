35219bf151dd6feaeab85a24b8370988ed14ea53
omarquaggiotto <omar.quaggiotto@gmail.com>
2026-09-15 13:31:30 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,54 +9,112 @@ const FILES_TO_CACHE = [
     "./assets/icon.svg"
 ];
 
+
+/* =========================================================
+   INSTALLAZIONE
+   ========================================================= */
+
 self.addEventListener("install", event => {
+
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
+
+        caches
+            .open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(
+                    FILES_TO_CACHE
+                );
+
+            })
+
     );
 
     self.skipWaiting();
+
 });
 
+
+/* =========================================================
+   ATTIVAZIONE
+   ========================================================= */
+
 self.addEventListener("activate", event => {
+
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            );
-        })
+
+        caches
+            .keys()
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys
+                        .filter(
+                            key =>
+                                key !== CACHE_NAME
+                        )
+                        .map(
+                            key =>
+                                caches.delete(key)
+                        )
+
+                );
+
+            })
+
     );
 
     self.clients.claim();
+
 });
 
+
+/* =========================================================
+   FETCH
+   ========================================================= */
+
 self.addEventListener("fetch", event => {
-    if (event.request.method !== "GET") return;
+
+    if (
+        event.request.method !== "GET"
+    ) {
+        return;
+    }
+
 
     event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
 
-            if (cachedResponse) {
-                return cachedResponse;
-            }
+        fetch(event.request)
 
-            return fetch(event.request)
-                .then(response => {
+            .then(response => {
 
-                    const responseClone = response.clone();
+                const responseClone =
+                    response.clone();
 
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, responseClone);
+                caches
+                    .open(CACHE_NAME)
+                    .then(cache => {
+
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+
                     });
 
-                    return response;
-                })
-                .catch(() => {
-                    return caches.match("./index.html");
-                });
-        })
+                return response;
+
+            })
+
+            .catch(() => {
+
+                return caches.match(
+                    event.request
+                );
+
+            })
+
     );
+
 });
